9f4946adaeb4041fbd4bf3bb36bdea85d1781bc6
Hassan Ezz <dhassanezz98@gmail.com>
2024-01-12 16:23:26 +0200

    feat: displayed compression ratio after compressing a file

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using System.IO;
+
 namespace HuffmanTree
 {
 
@@ -16,9 +17,28 @@ namespace HuffmanTree
             try
             {
                 if (args[0] == "d" || args[0] == "decode")
+                {
                     _ = new HuffmanDecoder(args[1], args[2]);
+
+                    FileInfo compressedFile = new(args[1]);
+                    FileInfo originalFile = new(args[2]);
+
+                    Console.WriteLine("Original file size: " + originalFile.Length);
+                    Console.WriteLine("Compressed file size: " + compressedFile.Length);
+                }
                 else
+                {
                     _ = new HuffmanEncoder(File.ReadAllText(args[0]), args[1]);
+
+                    FileInfo originalFile = new(args[0]);
+                    FileInfo compressedFile = new(args[1]);
+
+                    double compressionRate = (1 - compressedFile.Length / (double)originalFile.Length) * 100;
+
+                    Console.WriteLine("Original file size: " + originalFile.Length);
+                    Console.WriteLine("Compressed file size: " + compressedFile.Length);
+                    Console.WriteLine($"Compression rate: {compressionRate:F2}");
+                }
             }
             catch (Exception e)
             {
